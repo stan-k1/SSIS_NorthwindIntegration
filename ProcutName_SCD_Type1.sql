31155793a309b2cf5a5f50393a9cded60c3f8cef
Stan <sk2114@outlook.com>
2021-10-05 15:16:12 +0300

    Added Reference Script For Product Name Type 1 SCD

---
+++ ProcutName_SCD_Type1.sql
@@ -0,0 +1,40 @@
+﻿-- Type 1 SCD Merge Script For ProductName Column in Product/DimProduct 
+
+MERGE DimProduct AS target
+	USING IntergrationDimProduct as source
+	ON target.ProductId = source.ProductId
+ WHEN MATCHED
+ AND source.ProductName <> target.ProductName
+ THEN UPDATE SET
+	 target.ProductName = source.ProductName
+ WHEN NOT MATCHED THEN
+   INSERT  (
+	  [ProductID]
+      ,[ProductName]
+      ,[SupplierID]
+      ,[CategoryID]
+      ,[QuantityPerUnit]
+      ,[UnitPrice]
+      ,[UnitsInStock]
+      ,[UnitsOnOrder]
+      ,[ReorderLevel]
+      ,[Discontinued]
+      ,[CategoryName]
+      ,[CategoryDescription]
+   )
+   VALUES( 
+	  source.[ProductID]
+      ,source.[ProductName]
+      ,source.[SupplierID]
+      ,source.[CategoryID]
+      ,source.[QuantityPerUnit]
+      ,source.[UnitPrice]
+      ,source.[UnitsInStock]
+      ,source.[UnitsOnOrder]
+      ,source.[ReorderLevel]
+      ,source.[Discontinued]
+      ,source.[CategoryName]
+      ,source.[Description]
+   )
+WHEN NOT MATCHED BY Source THEN
+UPDATE SET Target.EndDate= CAST(GetDate() AS Date);
