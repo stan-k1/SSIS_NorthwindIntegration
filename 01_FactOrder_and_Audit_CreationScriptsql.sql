9ec04aa0c5d5dc2a8735266b7c5328b3598a1577
Stan <sk2114@outlook.com>
2021-10-06 16:18:55 +0300

    Added FactOrder

---
+++ 01_FactOrder_and_Audit_CreationScriptsql.sql
@@ -0,0 +1,28 @@
+﻿USE [IntegrationNW]
+GO
+
+CREATE TABLE [FactOrder] (
+    [OrderID] int,
+    [EmployeeID] int,
+    [OrderDate] datetime,
+    [RequiredDate] datetime,
+    [ShippedDate] datetime,
+    [ShipAddress] nvarchar(60),
+    [ShipCity] nvarchar(15),
+    [ShipRegion] nvarchar(15),
+    [ShipPostalCode] nvarchar(10),
+    [ShipCountry] nvarchar(15),
+    [ProductID] int,
+    [TotalFreight] money,
+    [AverageUnitPrice] money,
+    [AverageDiscountPercent] float
+)
+
+CREATE TABLE [dbo].[Audit](
+	[ProcessedTable] [nvarchar](64) NULL,
+	[LastRowKey] [int] NULL,
+    ExecutionTime datetime2 NULL,
+    Computer nvarchar(64) NULL,
+    Username nvarchar(64) NULL
+) ON [PRIMARY]
+GO
---
+++ Orders_ReadQuerry_For_FactOrder.sql
@@ -0,0 +1,12 @@
+﻿-- Reads data from Orders and Order Details, Aggregates to one row when both the Order Id and the Product Id are the same
+-- Uses Audit Table in the Where clause to only read new rows from the Orders table based on the id of the last processed order
+SELECT Orders.OrderID, ProductID, Orders.EmployeeID, Orders.OrderDate, Orders.RequiredDate, Orders.ShippedDate, Orders.ShipAddress, Orders.ShipCity, Orders.ShipRegion, Orders.ShipCountry, Orders.ShipPostalCode, 
+SUM(Freight) AS TotalFreight, AVG(UnitPrice) AS AverageUnitPrice, ROUND(AVG(Discount),2) * 100 AS AverageDiscountPercent
+FROM ORDERS
+LEFT JOIN [Order Details]
+ON Orders.OrderID = [Order Details].OrderID
+WHERE Orders.OrderID >
+	(SELECT MAX(LastRowKey)
+	FROM IntegrationNW.dbo.Audit
+	WHERE ProcessedTable='FactOrder')
+GROUP BY Orders.OrderID,  Orders.EmployeeID, Orders.OrderDate, Orders.RequiredDate, Orders.ShippedDate, Orders.ShipAddress, Orders.ShipCity, Orders.ShipRegion, Orders.ShipCountry, Orders.ShipPostalCode, ProductID
